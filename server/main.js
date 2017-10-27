9167d87ee63b5484ed7095e00d156815555da274
Ubuntu <ubuntu@ip-172-31-3-158.us-west-2.compute.internal>
2017-10-27 23:46:08 +0000

    test class

--- server/main.js
+++ server/main.js
@@ -2,5 +2,20 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/player';
 
 Meteor.startup(() => {
-  
+  class Person{
+    
+    constructor(name = 'Anonymous', age = 0) {
+      this.name = name;
+      this.age = age;
+    }
+    getGreeting() {
+      return `Hi, you are ${this.name}.`;
+    }
+    getPersonDes() {
+      return `${this.name} is ${this.age} year(s) old`;
+    }
+  } 
+  let me = new Person('Xuran');
+  //console.log(me.getGreeting());
+  console.log(me.getPersonDes());
 });
