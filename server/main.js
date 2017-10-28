cb6eb93067b45ca5623a0e3f8294bfc3a7afc305
Ubuntu <ubuntu@ip-172-31-3-158.us-west-2.compute.internal>
2017-10-28 00:07:24 +0000

    extends test

--- server/main.js
+++ server/main.js
@@ -14,8 +14,40 @@ Meteor.startup(() => {
     getPersonDes() {
       return `${this.name} is ${this.age} year(s) old`;
     }
+  }
+
+  class Employee extends Person {
+    constructor(name, age, title) {
+      super (name, age);
+      this.title = title;
+    }i
+    getGreeting() {
+      if (this.title) {
+        return `Hi, you are ${this.name}. You work as a ${this.title}`;
+      } else {
+        return super.getGreeting();
+      }
+    }
+    hasJob() {
+      return !!this.title;
+    }
+  }
+  class Programmer extends Person {
+    constructor (name, age, language = 'Java') {
+      super(name, age);
+      this.language = language;
+    }
+    getGreeting() {
+      return `Hi! I am ${this.name}, I use ${this.language}`
+    }
   } 
-  let me = new Person('Xuran');
-  //console.log(me.getGreeting());
-  console.log(me.getPersonDes());
+  let me = new Employee('Xuran', 26, 'db admin');
+  console.log(me.getGreeting());
+  //console.log(me.getPersonDes());
+  console.log(me.hasJob());
+  let you = new Employee('Mike');
+  console.log(you.getGreeting()); 
+
+  let prog = new Programmer();
+  console.log(prog.getGreeting());
 });
