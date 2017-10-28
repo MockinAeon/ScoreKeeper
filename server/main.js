8ad9bb2d7dfb98639a4ec75531ea8137f3b12a6c
Ubuntu <ubuntu@ip-172-31-3-158.us-west-2.compute.internal>
2017-10-28 21:25:19 +0000

    add react component

--- server/main.js
+++ server/main.js
@@ -2,52 +2,10 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/player';
 
 Meteor.startup(() => {
-  class Person{
-    
-    constructor(name = 'Anonymous', age = 0) {
-      this.name = name;
-      this.age = age;
+  let obj = {
+    name: 'Xuran',
+    printName() {
+      console.log(`Name: ${this.name}`);
     }
-    getGreeting() {
-      return `Hi, you are ${this.name}.`;
-    }
-    getPersonDes() {
-      return `${this.name} is ${this.age} year(s) old`;
-    }
-  }
-
-  class Employee extends Person {
-    constructor(name, age, title) {
-      super (name, age);
-      this.title = title;
-    }i
-    getGreeting() {
-      if (this.title) {
-        return `Hi, you are ${this.name}. You work as a ${this.title}`;
-      } else {
-        return super.getGreeting();
-      }
-    }
-    hasJob() {
-      return !!this.title;
-    }
-  }
-  class Programmer extends Person {
-    constructor (name, age, language = 'Java') {
-      super(name, age);
-      this.language = language;
-    }
-    getGreeting() {
-      return `Hi! I am ${this.name}, I use ${this.language}`
-    }
-  } 
-  let me = new Employee('Xuran', 26, 'db admin');
-  console.log(me.getGreeting());
-  //console.log(me.getPersonDes());
-  console.log(me.hasJob());
-  let you = new Employee('Mike');
-  console.log(you.getGreeting()); 
-
-  let prog = new Programmer();
-  console.log(prog.getGreeting());
+  };
 });
