5b680e696b3c5a4b1dccf99de609a3e8aa1ad627
Ubuntu <ubuntu@ip-172-31-3-158.us-west-2.compute.internal>
2017-10-29 00:33:34 +0000

    add App files for react

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -3,9 +3,13 @@ import Player from './Player';
 import PropTypes from 'prop-types';
 export default class PlayerList extends React.Component {
   renderPlayers() {
-    return this.props.players.map((player) => {
-      return <Player key={player._id} player={player}/>;
-    });
+    if (this.props.players.length == 0) {
+      return <p>你今天过得好吗？ 给我留个言吧!</p>
+    } else {
+      return this.props.players.map((player) => {
+        return <Player key={player._id} player={player}/>;
+      });
+    }
   }
   render() {
     return (
--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -2,18 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class TitleBar extends React.Component {
+  renderSubTitle() {
+    if (this.props.subt) {
+      return <h2>{this.props.subt}</h2>  
+    }
+  }
   render() {
     return (
       <div>
         <h1>{this.props.title}</h1>
-        <h2>{this.props.subt}</h2>
+        {this.renderSubTitle()}
       </div>
     );
   }
 }
 TitleBar.propTypes = {
   title: PropTypes.string.isRequired,
-  subt: PropTypes.string.isRequired
+  subt: PropTypes.string
 };
 
 
